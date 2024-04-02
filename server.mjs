c9fd2b29966ba82021c87c8f48aefedebea74eda
Kovid Krishnan <krishnankovid750@gmail.com>
2024-04-02 10:28:26 +0530

    Initial Commit for the server

---
+++ server.mjs
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch';
+
+const app = express();
+const PORT = 3000;
+
+const corsOptions = {
+  origin: ['127.0.0.1:5173', '192.168.29.83:5173']
+}
+
+app.use(cors()); // Use cors middleware with options
+
+app.use(express.json());
+
+app.post('/graphql', async (req, res) => {
+  const body = req.body;
+
+  fetch('https://leetcode.com/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Referer: 'https://leetcode.com'
+    },
+    body: JSON.stringify(body),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      res.json(data);
+    })
+    .catch(error => {
+      console.error('Error fetching or processing data:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
